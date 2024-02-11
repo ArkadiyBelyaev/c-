e3a83f48063711c5a06082b1f195b61a14127637
Arkady <arladibelyaev@gmail.com>
2024-02-11 22:21:03 +0300

    add new tasks

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -1,8 +1,56 @@
-﻿Console.WriteLine("Введите число");
+﻿// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = num / 100;
+// int num3 = num % 10;
+
+// int result = num1 * 10 + num3;
+// Console.WriteLine(result);
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.WriteLine());
+
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+
+// int num2 = num / 10 % 10;
+// int num3 = num % 10;
+// int result = 1;
+
+// while (num3 > 0)
+// {
+// 	result = result * num2;
+// 	num3--;
+// }
+
+// Console.WriteLine(result);
+// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 % num2 == 0)
+// {
+// 	Console.WriteLine("Yes");
+// }
+// else
+// {
+// 	Console.WriteLine($"No, {num1 % num2}");
+// }
+Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int num1 = num / 100;
-int num3 = num % 10;
-
-int result = num1 * 10 + num3;
-Console.WriteLine(result);
+if (num >= 100)
+{
+	int result = num / 100 % 10;
+	// while (num >= 1000)
+	// {
+	// 	num = num / 10;
+	// }
+	Console.WriteLine(result);
+}
+else
+{
+	Console.WriteLine("Третьей цифры нет");
+}
